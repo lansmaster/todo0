db74799468af216f6c33454a52610c3ba6354261
lansmaster <stepanbaryhev@gmail.com>
2024-12-20 00:06:24 +0300

    f

--- js/todo-functions.js
+++ js/todo-functions.js
@@ -3,12 +3,8 @@ import { saveTasksToStorage } from './storage.js';
 
 export function addTaskToList(taskName, todoList, tasks) {
 
-    const { li, doneButton, deleteButton } = createTodoItem(taskName);
+    const { li, deleteButton } = createTodoItem(taskName);
     todoList.append(li);
-
-    doneButton.addEventListener('click', () => {
-        li.classList.add('list-group-item-success');
-    });
 
     deleteButton.addEventListener('click', () => {
         const index = tasks.indexOf(taskName);
